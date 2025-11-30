256397b39880f97b3c88dcc4e67c36d6646c304b
Viktor84-code <viktor84.code@gmail.com>
2025-11-30 16:05:17 +0300

    feat: add date sorting logic with ascending/descending options

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ def main():
         print("Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING")
 
         while True:
-            status = input("Статус: ").upper()
+            status = input("Статус: ").strip().upper()
             if status in ["EXECUTED", "CANCELED", "PENDING"]:
                 print(f"Операции отфильтрованы по статусу '{status}'")
                 break
@@ -54,6 +54,20 @@ def main():
 
     else:
         print("Неверный выбор.")
+
+    # Сортировка по дате
+    sort_choice = input("Отсортировать операции по дате? Да/Нет: ").lower()
+    if sort_choice == "да":
+        sort_order = input("Отсортировать по возрастанию или по убыванию? ").lower()
+        if sort_order == "по возрастанию":
+            print("Сортировка по возрастанию даты")
+        elif sort_order == "по убыванию":
+            print("Сортировка по убыванию даты")
+        else:
+            print("Неверный выбор сортировки")
+    else:
+        print("Сортировка по дате не применяется")
+
 
 if __name__ == "__main__":
     main()
